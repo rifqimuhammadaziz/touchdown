4e8565155ea626ac42e2e081530637e6159516af
Rifqi Muhammad Aziz <IDS.RIFQIM@xl.co.id>
2023-04-14 22:19:52 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Rifqi Muhammad Aziz on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
