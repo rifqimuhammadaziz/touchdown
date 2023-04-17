959df2bb80f6aed4000cd12d7d0996ea52dcf60d
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-17 10:25:06 +0700

    create product detail view

---
+++ Touchdown/View/Detail/HeaderDetailView.swift
@@ -0,0 +1,30 @@
+//
+//  HeaderDetailView.swift
+//  Touchdown
+//
+//  Created by Rifqi Muhammad Aziz on 17/04/23.
+//
+
+import SwiftUI
+
+struct HeaderDetailView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6, content: {
+            Text("Protective Gear")
+            
+            Text(sampleProduct.name)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+        }) //: VSTACK
+        .foregroundColor(.white)
+    }
+}
+
+struct HeaderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
+}
