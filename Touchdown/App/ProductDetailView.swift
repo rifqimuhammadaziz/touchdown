b52fcaf268f1beeadd998d3e58216172575da357
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-17 22:28:32 +0700

    finish up product detail view

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -25,18 +25,39 @@ struct ProductDetailView: View {
             // DETAIL TOP PART
             TopPartDetailView()
                 .padding(.horizontal)
+                .zIndex(1)
             
             // DETAIL BOTTOM PART
-            
-            // RATING + SIZE
-            
-            // DESCRIPTION
-            
-            // QTY + FAVOURITE
-            
-            // ADD TO CART
-            Spacer()
+            VStack(alignment: .center, spacing: 0, content: {
+                // RATING + SIZE
+                RatingsSizesDetailView()
+                    .padding(.top, -20)
+                    .padding(.bottom, 10)
+                
+                // DESCRIPTION
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                }) //: SCROLL
+                
+                // QTY + FAVOURITE
+                QuantityFavouriteDetailView()
+                    .padding(.vertical, 10)
+                
+                // ADD TO CART
+                AddToCartDetailView()
+                    .padding(.bottom, 20)
+            }) //: VSTACK
+            .padding(.horizontal)
+            .background(
+                Color.white
+                    .clipShape(CustomShape())
+                    .padding(.top, -105)
+            )
         }) //: VSTACK
+        .zIndex(0)
         .ignoresSafeArea(.all, edges: .all)
         .background(
             Color(
